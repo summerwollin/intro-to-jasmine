c5811a19a8a43360dbd752b464ed450b6cfbe2fc
Summer Wollin <summer.wollin@gmail.com>
2016-02-24 13:16:19 -0700

    Include test for if child is too short, returns false

--- spec/rides_spec.js
+++ spec/rides_spec.js
@@ -1,8 +1,16 @@
 var rides = require('../lib/rides')
 
 describe("Rides", function(){
-  it('determines if child is tall enough for the ride', function(){
-
-  expect(rides.isTallEnough()).toEqual(false);
+  it('determines when a child is too short', function(){
+    var child1 = {
+      height: 3,
+      name: "Julian",
+      age: 3
+    };
+    var ride2 = {
+      minHeight: 4,
+      maxHeight: 7
+    };
+  expect(rides.isTallEnough(child1,ride2)).toEqual(false);
   });
 });
