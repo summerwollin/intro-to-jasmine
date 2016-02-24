b438eedce293c93eeb506e354b0fc23e54e13c39
Summer Wollin <summer.wollin@gmail.com>
2016-02-24 13:18:54 -0700

    Add test case for if child is tall enough, test running red

--- spec/rides_spec.js
+++ spec/rides_spec.js
@@ -13,4 +13,17 @@ describe("Rides", function(){
     };
   expect(rides.isTallEnough(child1,ride2)).toEqual(false);
   });
+  it('determines when a child is tall enough', function() {
+    var child2 = {
+      height: 4,
+      name: "Cero",
+      age: 4
+    };
+    var ride1 = {
+      minHeight: 3,
+      maxHeight: 7
+    };
+
+  expect(rides.isTallEnough(child2,ride1)).toEqual(true);
+  });
 });
